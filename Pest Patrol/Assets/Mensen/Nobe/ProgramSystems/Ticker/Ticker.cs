57d17059bb773d04850014e06df81f01c5a6f730
SedKety <nobe.mol@gmail.com>
2024-09-09 13:58:03 +0200

    Files/Ticker

---
+++ Pest Patrol/Assets/Mensen/Nobe/ProgramSystems/Ticker/Ticker.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ticker : MonoBehaviour
+{
+    public float tickTime;
+
+    public float tickerTimer;
+
+    public delegate void TickAction();
+    public static event TickAction OnTickAction;
+
+    void Update()
+    {
+        tickerTimer += Time.deltaTime;
+        if (tickerTimer >= tickTime)
+        {
+            tickerTimer = 0;
+            TickEvent();
+        }
+    }
+
+    void TickEvent()
+    {
+        OnTickAction?.Invoke();
+    }
+}
