b85a2c739684c90fa96b3c332920c231d75bab81
PatrickkikALT <45902985+PatrickkikALT@users.noreply.github.com>
2024-10-14 13:36:08 +0200

    EscapeMenu turns off ui + now uses events.

--- Pest Patrol/Assets/EscapeMenu.cs
+++ Pest Patrol/Assets/EscapeMenu.cs
@@ -5,59 +5,50 @@ using System.Linq;
 using UnityEngine;
 using UnityEngine.EventSystems;
 using UnityEngine.SceneManagement;
+using UnityEngine.Events;
 
 public class EscapeMenu : MonoBehaviour
 {
+    public delegate void ToggleUI(bool state);
+    public static event ToggleUI OnEscape;
     public GameObject[] uiObjects;
     public LayerMask layer;
     public GameObject[] activeUI;
-    //TODO: make this not broken lmao, uiObjects always returns 0 even though I dont see any issues with the expressions and it should return all the UI objects.
     private void Start()
     {
-        uiObjects = FindObjectsOfType<GameObject>(true).Where(x => x.layer == (int)layer.value).ToArray();
         activeUI = uiObjects.Where(x => x.activeInHierarchy).ToArray();
+        OnEscape += SetUIActive;
     }
     
-    public void TurnOffUI()
+    public void SetUIActive(bool state)
     {
-        var uiToTurnOff = activeUI.Where(x => x != gameObject || x.transform.IsChildOf(gameObject.transform)).ToArray();
-        foreach (var ui in uiToTurnOff)
-        {
-            ui.SetActive(false);
-            print(ui.name);
-        } 
-    }
-    
-    public void TurnOnUI()
-    {
-        var uiToTurnOn = activeUI.Where(x => x != gameObject || x.transform.IsChildOf(gameObject.transform)).ToArray();
-        foreach (var ui in uiToTurnOn) ui.SetActive(true);
-        gameObject.transform.GetChild(0).gameObject.SetActive(false);
+        foreach (var ui in activeUI) ui.SetActive(state);
+        gameObject.transform.GetChild(0).gameObject.SetActive(!state);
     }
     
     public void Update()
     {
         if (!Input.GetKeyDown(KeyCode.Escape)) return;
         Time.timeScale = 0;
-        TurnOffUI();
+        OnEscape?.Invoke(false);
         gameObject.transform.GetChild(0).gameObject.SetActive(true);
     }
 
     public void ResumeGame()
     {
-        TurnOnUI();
+        OnEscape?.Invoke(true);
         Time.timeScale = 1;
     }
 
     public void SettingsButton(GameObject settingsMenu)
     {
         settingsMenu.SetActive(true);
-        GameObject button = EventSystem.current.currentSelectedGameObject;
+        var button = EventSystem.current.currentSelectedGameObject;
         button.transform.parent.gameObject.SetActive(false);
     }
 
     public void QuitButton()
     {
-        SceneManager.LoadScene("Patrick");
+        SceneManager.LoadScene("MainMenu");
     }
 }
