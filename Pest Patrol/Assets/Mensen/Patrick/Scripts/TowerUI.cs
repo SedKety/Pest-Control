dc375ecf2963c37bc26db371dd4160d1e0d7c87e
PatrickkikALT <45902985+PatrickkikALT@users.noreply.github.com>
2024-10-25 14:12:59 +0200

    few last bug fixes
    game over works in city

--- Pest Patrol/Assets/Mensen/Patrick/Scripts/TowerUI.cs
+++ Pest Patrol/Assets/Mensen/Patrick/Scripts/TowerUI.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -7,10 +8,19 @@ public class TowerUI : MonoBehaviour
     public TowerUpgrading upgradePanel;
     public EnemyStats enemyPanel;
     public LayerMask triggerLayer;
+    public LayerMask uiLayer;
+    private Camera main;
+
+    private void Start()
+    {
+        main = Camera.main;
+    }
+
     void Update()
     {
         if (!Input.GetMouseButtonDown(0)) return;
-        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        Ray ray = main.ScreenPointToRay(Input.mousePosition);
+        if (Physics.Raycast(ray, Mathf.Infinity, uiLayer)) return;
         if (!Physics.Raycast(ray, out RaycastHit hit, Mathf.Infinity, triggerLayer)) return;
         if (hit.collider.GetComponent<CombatTower>() != null)
         {
